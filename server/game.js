185e4c3a1cae25fa2dc11a69888446f8af95068e
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2015-08-09 22:27:59 +0200

    Moved functionality from server.js to game.js

--- server/game.js
+++ server/game.js
@@ -1,14 +1,82 @@
-// TOOD start/restartGame a game, tracks players alive, determine winner, trigger countdown...
-
 module.exports = Game;
 
 var Uuid = require('uuid');
+var C = require("./config");
+var World = require("./world.js");
 var Player = require("./player.js");
 
 
-function Game() {
+function Game(io, onWorldRestart) {
+	this.world = new World(C.WORLD.WIDTH, C.WORLD.HEIGHT, C.PLAYER.SIZE, onWorldRestart);
+	this.countingDown = false;
+	this.io = io;		// TODO Remove this.io
 }
 
-World.prototype.resetGrid = function() {
-	console.log("Reset Grid");
-	for (var x = 0; x < this.tiles_width; x++)
+Game.prototype.update = function(player) {
+	if(this.countingDown || this.world.getTotalPlayers() < 1) {
+    return;
+  }
+	this.world.update();
+
+  if(this.world.restartWhenAllPlayersDied()) {
+    this.startNewGame(3);
+  } else {
+    this.countingDown = false;
+    this.io.emit('render', {
+      totalPlayers: this.world.getTotalPlayers(),
+      world: this.world
+    });
+  }
+};
+
+
+Game.prototype.playerMoved = function(playerId, newDirection) {
+    var p = this.world.players[playerId];
+    switch (newDirection) {
+      case C.DIRECTION.LEFT:
+        p.left();
+        break;
+      case C.DIRECTION.UP:
+        p.up();
+        break;
+      case C.DIRECTION.RIGHT:
+        p.right();
+        break;
+      case C.DIRECTION.DOWN:
+        p.down();
+        break;
+    }
+};
+
+
+Game.prototype.togglePause = function() {
+	this.world.togglePause();
+};
+
+
+Game.prototype.startNewGame = function(seconds) {
+  if(this.countingDown) {
+    // Already starting a new Game.
+    return;
+  }
+  this.world.restart();
+  this.countingDown = true;
+  var _this = this;
+  var intervalId = setInterval(function() {
+	   if(seconds > 0) {
+	      // console.log('Start new game in:', seconds);
+	      _this.io.emit('countdown', {
+	        seconds: seconds
+	      });
+	      seconds--;
+	    } else {
+	      _this.io.emit('countdown', {
+	        seconds: 'GO'
+	      });
+	      clearInterval(intervalId);
+	      _this.countingDown = false;
+	      // Start the game!
+	      _this.world.pause(false);
+	    }  	
+  }, 1000);
+};
--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ var port = process.env.PORT || 3000;
 var host = process.env.HOST || 'localhost';
 var Player = require("./player.js");
 var World = require("./world.js");
+var Game = require("./game.js");
 var C = require("./config");
 
-var world;
-var countingDown = false;
+var game;
 
 // Routing
 app.use(express.static(__dirname + '/../3d-client'));
@@ -21,70 +21,32 @@ app.use('/2d', express.static(__dirname + '/../client'));
 server.listen(port, function() {
   console.log('Server listening at %s:%s', host, port);
   console.log("Settings; ", C);
-  world = new World(C.WORLD.WIDTH, C.WORLD.HEIGHT, C.PLAYER.SIZE, onWorldRestart);
+  // world = new World(C.WORLD.WIDTH, C.WORLD.HEIGHT, C.PLAYER.SIZE, onWorldRestart);
+  game = new Game(io, onWorldRestart);
   setInterval(loop, C.TICK_TIME);
 });
 
 
 // Main loop, will be called every 'X' time, see C.TICK_TIME
 function loop() {
-  if(countingDown || world.getTotalPlayers() < 1) {
-    return;
-  }
-  world.update();
-  if(world.restartWhenAllPlayersDied()) {
-    startNewGame(world, 3);
-  } else {
-    countingDown = false;
-    io.emit('render', {
-      totalPlayers: world.getTotalPlayers(),
-      world: world
-    });
-  }
+  game.update();
 }
-
-var startNewGame = function(world, seconds) {
-  if(countingDown) {
-    // Already starting a new Game.
-    return;
-  }
-  world.restart();
-  countingDown = true;
-  var intervalId = setInterval(function() {
-    if(seconds > 0) {
-      // console.log('Start new game in:', seconds);
-      io.emit('countdown', {
-        seconds: seconds 
-      });
-      seconds--;
-    } else {
-      io.emit('countdown', {
-        seconds: 'GO'
-      });
-      clearInterval(intervalId);
-      countingDown = false;
-      // Start the game!
-      world.pause(false);
-    }
-  }, 1000);
-
-};
 
 
 function clientConnected(socket) {
-  var player = world.createPlayer();
+  var player = game.world.createPlayer();
   socket.userId = player.id;
 
   socket.emit('connected', {
     id: player.id,
-    totalPlayers: world.getTotalPlayers(),
-    world: world
+    totalPlayers: game.world.getTotalPlayers(),
+    world: game.world
   });
 }
 
 function onWorldRestart() {
   io.emit('restart', {
-    world: world
+    world: game.world
   });
 }
 
@@ -94,46 +56,32 @@ io.on('connection', function(socket) {
 
   socket.broadcast.emit('user joined', {
     player: socket.userId,
-    totalPlayers: world.getTotalPlayers(),
-    world: world
+    totalPlayers: game.world.getTotalPlayers(),
+    world: game.world
   });
 
 
   socket.on('restart', function() {
-    startNewGame(world, 3);
+    game.startNewGame(3);
   });
 
   socket.on('toggle pause', function() {
-    world.togglePause();
+    game.togglePause();
   });
 
   socket.on('change direction', function(newDirection) {
-    var p = world.players[socket.userId];
-    switch (newDirection) {
-      case C.DIRECTION.LEFT:
-        p.left();
-        break;
-      case C.DIRECTION.UP:
-        p.up();
-        break;
-      case C.DIRECTION.RIGHT:
-        p.right();
-        break;
-      case C.DIRECTION.DOWN:
-        p.down();
-        break;
-    }
+    game.playerMoved(socket.userId, newDirection);
   });
 
   socket.on('disconnect', function() {
     // remove the userId from global userIds list
     console.log(socket.userId + " disconnected");
-    world.removePlayer(socket.userId);
+    game.world.removePlayer(socket.userId);
 
     socket.broadcast.emit('user left', {
       player: socket.userId,
-      totalPlayers: world.getTotalPlayers(),
-      world: world
+      totalPlayers: game.world.getTotalPlayers(),
+      world: game.world
     });
   });
 
