9a477bc21cfc43f161c0d6b4856587bd8a13ac50
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2015-09-10 22:12:23 +0200

    Added code for (3d)client to Docker container

--- server/docker-run.sh
+++ server/docker-run.sh
@@ -1,5 +1,9 @@
 #docker run -p 3000:3000 rachidbm/lightbikes-server
 echo "IP address of Docker containers: `boot2docker ip`";
-docker --tlsverify=false run -p 3000:3000 rachidbm/lightbikes-server
+#docker --tlsverify=false run -p 3000:3000 rachidbm/lightbikes-server
+docker --tlsverify=false run -p 3000:3000  \
+	-v "$PWD/../3d-client":/usr/src/3d-client \
+	-v "$PWD/../client":/usr/src/client \
+	rachidbm/lightbikes-server 
 
 ## Run the server on a different port : -p 9000:3000
